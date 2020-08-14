acf93c6075a6c7e23a6dc679e535551009936748
ak692 <ak69211@gmail.com>
2020-08-14 00:18:36 -0400

    filter out records for which link is unavailable

--- bhajancrawler/bhajancrawler/spiders/bhajan_spider.py
+++ bhajancrawler/bhajancrawler/spiders/bhajan_spider.py
@@ -13,8 +13,6 @@ from scrapy.linkextractors import LinkExtractor
 class BhajanSpider(CrawlSpider):
 
     name = "Bhajans"
-    # allowed_domains = ['sairhythms.org/song']
-    # base_url = ['"https:/www.sairhythms.org/']
 
     start_urls = [
         'https://sairhythms.org/'
@@ -29,14 +27,14 @@ class BhajanSpider(CrawlSpider):
         def extract_with_xpath(query):
             return response.xpath(query).xpath('string(.)').get()
 
-        return {
+        bhajan_info = {
             'link': response.xpath("""//div[contains(@class, 'audio-link')]//a[1]""").xpath('text()').get(),
             'deity': extract_with_xpath("""//div[contains(@class, 'deity-row')]//div[2]"""),
             'language': extract_with_xpath("""//div[contains(@class, 'language-row')]//div[2]"""),
             'raaga': extract_with_xpath("""//div[contains(@class, 'raga-title')]"""),
-            # excludes other ragas for songs with multiple ragas (only includes first)
-            # 'raga': extract_with_xpath("""//div[contains(@class, 'raga-row')]//div[2]"""), # This gets all text but includes noise
             'beat': extract_with_xpath("""//div[contains(@class, 'beat-row')]//div[2]"""),
             'level': extract_with_xpath("""//div[contains(@class, 'level-row')]//div[2]"""),
             'tempo': extract_with_xpath("""//div[contains(@class, 'tempo-row')]//div[2]""")
         }
+        if bhajan_info['link'] is not None:
+            yield bhajan_info
